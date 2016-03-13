da2d917e89ea12a7d7a9e94544549b68c6ea2c73
hostship <khaled@uvic.ca>
2016-03-13 18:45:28 +0000

    homepage creation

--- application/views/home.php
+++ application/views/home.php
@@ -1,9 +1,40 @@
 <?= $header ?>
-	<div class="w-section mainslider">
-		<div class="w-container">
-			<h1 class="mainheader">Matching employers who value diversity with talented disabled candidates<br>in Victoria, BC, Canada.</h1>
-			<div class="w-clearfix cta"><a href="#" class="w-button sbutton">Candidates</a><a href="#" class="w-button fbutton">Job Opportunities</a>
-			</div>
-		</div>
-	</div>
+	<?= $header ?>
+
+
+
+	  <div class="w-section mainslider">
+    <div class="w-container">
+      <h1 class="mainheader">Matching employers who value diversity with talented disabled candidates<br>in Victoria, BC, Canada.</h1>
+      <div class="w-clearfix cta"><a href="#" class="w-button sbutton">Candidates</a><a href="#" class="w-button fbutton">Job Opportunities</a>
+      </div>
+    </div>
+  </div>
+  <div class="w-section logo-section">
+    <div class="w-container"><img src="images/logo_Wired.svg" class="slogo"><img src="images/logo_Forbes.svg" class="slogo"><img src="images/logo_techcrunch.svg" class="slogo"><img height="40" src="images/logo_zdnet.svg" class="slogo">
+      <div class="textlogo">
+        <div class="thinpart"><strong class="thinpart">Hack</strong><span class="boldedpart">Victoria</span>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-section section1">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-6">
+          <h2 class="homeheader2">Why it is good for business ?</h2>
+          <p class="firsttext">Despite the cost involved in accomodating a disabled person in your company, &nbsp;A diverse workforce gives you a competitive advantage by adding new ideas, viewpoints and approaches to solving your business challenges.
+            <br>
+            <br>Retention rates among people with disabilities are higher because they are loyal to companies that supports them. Thus reducing the training costs.</p>
+        </div>
+        <div class="w-col w-col-6"><img src="images/post-1-0-55919700-1384422697_thumb.jpg" class="sideimage">
+        </div>
+      </div>
+    </div>
+  </div>
+	
+	
+	
+	
+<?= $footer ?>
 <?= $footer ?>
