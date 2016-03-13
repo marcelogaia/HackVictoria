075cdfcb29761841221a39578ce93301843b44ce
hostship <khaled@uvic.ca>
2016-03-13 13:06:46 +0000

    adding the create profile html page

---
+++ application/views/create.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<!-- This site was created in Webflow. http://www.webflow.com-->
+<!-- Last Published: Sun Mar 13 2016 13:02:18 GMT+0000 (UTC) -->
+<html data-wf-site="5634ecbff1efd346405c7d64" data-wf-page="56e54086b4a0d57a57af537f">
+<head>
+  <meta charset="utf-8">
+  <title>create</title>
+  <meta property="og:title" content="create">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="generator" content="Webflow">
+  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+  <link rel="stylesheet" type="text/css" href="css/webflow.css">
+  <link rel="stylesheet" type="text/css" href="css/testwebsitetest1.webflow.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+  <script>
+    WebFont.load({
+      google: {
+        families: ["Exo:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic","Lato:100,100italic,300,300italic,400,400italic,700,700italic,900,900italic","Roboto:300,regular,500","Roboto Condensed:300,regular,700","Roboto Slab:300,regular,700","Arbutus Slab:regular"]
+      }
+    });
+  </script>
+  <script type="text/javascript" src="js/modernizr.js"></script>
+  <link rel="shortcut icon" type="image/x-icon" href="images/New icon.ico">
+  <link rel="apple-touch-icon" href="images/metric-webclip.png">
+</head>
+<body>
+  <div data-collapse="medium" data-animation="default" data-duration="400" data-contain="1" class="w-nav navigation">
+    <div class="w-container">
+      <a href="index.html" class="w-nav-brand brand-link">
+        <div class="logo-text">Victoria</div>
+      </a>
+      <nav role="navigation" class="w-nav-menu nav-menu"><a href="#features" class="w-nav-link nav-link">Home</a><a href="#customers" class="w-nav-link nav-link">Profiles</a><a href="#customers" class="w-nav-link nav-link">Contact us</a>
+        <div class="personal">
+          <div class="profile-pic"><img width="45" src="images/facebook-teerasej-profile-ball-circle.png">
+          </div>
+          <div data-delay="0" class="w-dropdown drop-down">
+            <div class="w-dropdown-toggle test2">
+              <div class="person-name">Smith J. William</div>
+              <div class="w-icon-dropdown-toggle arrow"></div>
+            </div>
+            <nav class="w-dropdown-list list"><a href="http://manageprofile.php" class="w-dropdown-link indi-list">Manage Profile</a><a href="http://logout.php" class="w-dropdown-link indi-list">Logout</a>
+            </nav>
+          </div>
+        </div>
+      </nav>
+      <div class="w-nav-button nav-link menu">
+        <div class="w-icon-nav-menu"></div>
+      </div>
+    </div>
+  </div>
+  <div class="w-section sing-in-section">
+    <div class="w-container">
+      <div class="create-profile">
+        <h1 class="sign-in-word">Create your profile</h1>
+        <div class="little-message">Showcase what you skills and qualifications to everybody.</div>
+        <div class="w-form">
+          <form id="email-form" name="email-form" data-name="Email Form" method="post">
+            <h2 class="subhead">Basic Information:</h2>
+            <div class="w-row">
+              <div class="w-col w-col-4">
+                <label for="name" class="form-label">Name:</label>
+                <input id="name" type="email" placeholder="Enter your name" name="name" data-name="Name" class="w-input">
+              </div>
+              <div class="w-col w-col-4">
+                <label for="location" class="form-label">Mobile Number:</label>
+                <input id="location" type="password" placeholder="Enter your mobile number" name="location" data-name="location" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-4">
+                <label for="email-2" class="form-label">Date of birth :</label>
+                <input id="email-2" type="password" placeholder="Enter your date of birth" name="email-2" data-name="Email 2" required="required" class="w-input">
+              </div>
+            </div>
+            <label for="field-3" class="form-label">Short Summary:</label>
+            <textarea id="field-3" placeholder="Enter a short description of yourself" name="field-3" data-name="Field 3" class="w-input"></textarea>
+            <h2 class="subhead">Location:</h2>
+            <div class="w-row">
+              <div class="w-col w-col-3">
+                <label for="location-8" class="form-label">Zip Code:</label>
+                <input id="location-8" type="email" placeholder="Enter your zip code" name="location-1" data-name="location 1" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="Location-7" class="form-label">Appartment number:</label>
+                <input id="Location-7" type="password" placeholder="Enter appatment number" name="Location-2" data-name="Location 2" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="location-9" class="form-label">Street number:</label>
+                <input id="location-9" type="password" placeholder="Enter your street number" name="location-3" data-name="location 3" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="location-10" class="form-label">Street name:</label>
+                <input id="location-10" type="password" placeholder="Enter your street name" name="location-4" data-name="location 4" required="required" class="w-input">
+              </div>
+            </div>
+            <div class="w-row">
+              <div class="w-col w-col-3">
+                <label for="location-11" class="form-label">Neighborhood:</label>
+                <input id="location-11" type="email" placeholder="Enter your neibhorhood" name="location-5" data-name="location 5" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="Location-12" class="form-label">City:</label>
+                <input id="Location-12" type="password" placeholder="Enter your city" name="Location-6" data-name="Location 6" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="location-13" class="form-label">Province:</label>
+                <input id="location-13" type="password" placeholder="Enter your location" name="location-7" data-name="location 7" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-3"></div>
+            </div>
+            <h2 class="subhead">Experiences:</h2>
+            <div class="w-row">
+              <div class="w-col w-col-3">
+                <label for="name-2" class="form-label">Title:</label>
+                <input id="name-2" type="email" placeholder="What was your job title" name="name-2" data-name="Name 2" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="location-2" class="form-label">Company:</label>
+                <input id="location-2" type="password" placeholder="Enter the company's name" name="location-2" data-name="Location 2" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="email-3" class="form-label">Start Date :</label>
+                <input id="email-3" type="password" placeholder="Enter your start date" name="email-3" data-name="Email 3" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="email-4" class="form-label">End Date :</label>
+                <input id="email-4" type="password" placeholder="Enter your end date" name="email-4" data-name="Email 4" required="required" class="w-input">
+              </div>
+            </div>
+            <label for="field" class="form-label">Description:</label>
+            <textarea id="field" placeholder="Enter a short description of your self" name="field" class="w-input"></textarea>
+            <h2 class="subhead">Education:</h2>
+            <div class="w-row">
+              <div class="w-col w-col-3">
+                <label for="name-3" class="form-label">Title:</label>
+                <input id="name-3" type="email" placeholder="What was your job title" name="name-3" data-name="Name 3" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="location-3" class="form-label">Company:</label>
+                <input id="location-3" type="password" placeholder="Enter the company's name" name="location-3" data-name="Location 3" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="email-5" class="form-label">Start Date :</label>
+                <input id="email-5" type="password" placeholder="Enter your start date" name="email-5" data-name="Email 5" required="required" class="w-input">
+              </div>
+              <div class="w-col w-col-3">
+                <label for="email-6" class="form-label">End Date :</label>
+                <input id="email-6" type="password" placeholder="Enter your end date" name="email-6" data-name="Email 6" required="required" class="w-input">
+              </div>
+            </div>
+            <label for="field" class="form-label">Description:</label>
+            <textarea id="field-2" placeholder="Enter a short description of your self" name="field-2" data-name="Field 2" class="w-input"></textarea>
+            <h2 class="subhead">Dissabilitees:</h2>
+            <label for="field-4" class="form-label">Describe What is your dissablities</label>
+            <textarea id="field-4" placeholder="Describe in few sentences your disabilities." name="field-4" data-name="Field 4" class="w-input"></textarea>
+            <h2 class="subhead">Skills:</h2>
+            <label for="field-4" class="form-label">Enter a list of words that describes your skills.</label>
+            <textarea id="field-5" placeholder="Seperate your skills by comma, writing more skills make your resume more searchable" name="field-5" data-name="Field 5" class="w-input"></textarea>
+            <input type="submit" value="Submit" data-wait="Please wait..." class="w-button sign-in-button">
+          </form>
+          <div class="w-form-done psm">
+            <p>Loading ....</p>
+          </div>
+          <div class="w-form-fail">
+            <p>Oops! Something went wrong when submiting your resume</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-section section footer">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="w-col w-col-6">
+          <div class="logo-text footer">OPEN GATE</div>
+          <div class="footer-slogan">Sign up for our newsletter</div>
+          <div class="w-form newsletter-form">
+            <form id="wf-form-newsletter" name="wf-form-newsletter" data-name="Newsletter" class="w-clearfix">
+              <input id="email-2" type="email" placeholder="Email Address" name="email-2" data-name="Email 2" required="required" class="w-input newsletter-field">
+              <input type="submit" value="Subscribe" data-wait="Wait..." wait="Wait..." class="w-button newsletter-button">
+            </form>
+            <div class="w-form-done success-message">
+              <p>Thank you! You’ll receive an email shortly.</p>
+            </div>
+            <div class="w-form-fail">
+              <p>Oops! Something went wrong while submitting the form :(</p>
+            </div>
+          </div>
+          <div class="social-icon-group">
+            <a href="#" class="w-inline-block social-icon"><img src="images/facebook-icon.svg">
+            </a>
+            <a href="#" class="w-inline-block social-icon"><img src="images/twitter-icon.svg">
+            </a>
+            <a href="#" class="w-inline-block social-icon"><img src="images/linkdin-icon-white.svg">
+            </a>
+            <a href="#" class="w-inline-block social-icon"><img src="images/email-icon-white.svg">
+            </a>
+          </div>
+        </div>
+        <div class="w-col w-col-2">
+          <h4 class="footer-title">Company</h4><a href="#" class="page-link in-footer">Home</a><a href="#" class="page-link in-footer">Profiles</a><a href="#" class="page-link in-footer">Job Opportunities</a><a href="#" class="page-link in-footer">Sign in</a><a href="#" class="page-link in-footer">Sign Up</a>
+        </div>
+        <div class="w-col w-col-2">
+          <h4 class="footer-title">Resources</h4><a href="#" class="page-link in-footer">Blog</a><a href="#" class="page-link in-footer">Employer Guide</a><a href="#" class="page-link in-footer">Employee Guide</a>
+        </div>
+        <div class="w-col w-col-2">
+          <h4 class="footer-title">Legal</h4><a href="#" class="page-link in-footer">Privacy Policy</a><a href="#" class="page-link in-footer">Terms of Use</a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="w-section section footer copyright">
+    <div class="w-container">
+      <div>Copyright 2014 Hack Victoria. All Rights Reserved.</div>
+    </div>
+  </div>
+  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+  <script type="text/javascript" src="js/webflow.js"></script>
+  <!--[if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif]-->
+</body>
+</html>
